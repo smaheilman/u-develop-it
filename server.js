d1a41390f2664d77ab43828fb22579dd9f23d480
Shmee <smaheilman@gmail.com>
2021-11-18 21:11:31 -0500

    rows

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '7Rdcmnse.',
+        database:'election'
+    },
+    console.log('connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (NOT FOOUNDDD)
 app.use((req,res) => {

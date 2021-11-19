a6f82d71692cf37459e96c7fcbb2f1bec3a92f02
Shmee <smaheilman@gmail.com>
2021-11-18 21:25:17 -0500

    queries

--- server.js
+++ server.js
@@ -16,8 +16,36 @@ const db = mysql.createConnection(
     console.log('connected to the election database.')
 );
 
-db.query('SELECT * FROM candidates', (err, rows) => {
-    console.log(rows);
+//db.query('SELECT * FROM candidates', (err, rows) => {
+//    console.log(rows);
+//});
+
+//Get a solo candidate
+//db.query('SELECT * From candidates where id=1', (err,row)=> {
+//    if (err) {
+//        console.log(err);
+//    }
+//    console.log(row);
+//});
+
+//Delete a candidate
+//db.query('DELETE from candidates where id=?',1,(err, result) => {
+//    if(err){
+//        console.log(err);
+//    }
+//    console.log(result);
+//});
+
+//Create a candidate
+const sql = `insert into candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result)=> {
+    if(err){
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //Default response for any other request (NOT FOOUNDDD)
